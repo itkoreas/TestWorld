c6e5421c56e85c838f0a27058d31bc668cf9e248
itkoreas <60996426+itkoreas@users.noreply.github.com>
2020-02-13 16:28:08 +0900

    Update HelloGitWorld.java

--- HelloGitWorld.java
+++ HelloGitWorld.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Git Test");
 
-               fdgfgdfdfdffdfsadffds
+              // fdgfgdfdfdffdfsadffds
 }} 
 
 		System.out.println("Testing....in GitHub");
